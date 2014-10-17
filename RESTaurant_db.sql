3f2df560b52167ac56778a57719663842a43c9b1
Holderness <andersonholderness@gmail.com>
2014-10-17 14:14:04 -0400

    touches before code freeze

--- RESTaurant_db.sql
+++ RESTaurant_db.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE RESTaurant_db;
 
-\c RESTaurant_db;
+\c restaurant_db;
 
 CREATE TABLE foods (
 id SERIAL PRIMARY KEY,
@@ -14,14 +14,14 @@ CREATE TABLE parties (
 id SERIAL PRIMARY KEY,
 table_no INTEGER,
 guest_no INTEGER,
-paid BOOLEAN
+paid BOOLEAN,
+dufresnes BOOLEAN
 );
 
 CREATE TABLE orders (
 id SERIAL PRIMARY KEY,
 food_id INTEGER,
 party_id INTEGER,
-dufresnes BOOLEAN
 );
 
 
